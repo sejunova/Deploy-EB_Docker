1d1fb767f7a23f6f02e125368b2d169083576f64
Sejun Im <sejun425@gmail.com>
2017-11-03 15:09:32 +0900

    elastic beanstalk deploy ready

--- instagram/config/settings/dev.py
+++ instagram/config/settings/dev.py
@@ -1,16 +1,23 @@
 from .base import *
 
+
+ALLOWED_HOSTS = [
+    '.elasticbeanstalk.com',
+    '.isj.co.kr',
+]
+
 config_secret_dev = json.loads(open(CONFIG_SECRET_DEV).read())
 
 DATABASES = config_secret_dev['django']['databases']
 
+SECRET_KEY = config_secret_dev['django']['secret_key']
 
 
-AWS_ACCESS_KEY_ID = config_secret_common['aws']['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = config_secret_common['aws']['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = config_secret_common['aws']['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = config_secret_dev['aws']['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = config_secret_dev['aws']['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = config_secret_dev['aws']['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_REGION_NAME_= 'ap-northeast-2'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 # S3 FileStorage
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
